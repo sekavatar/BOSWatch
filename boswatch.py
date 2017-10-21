d519d16e50c0e1ecfc212e2c4618d29861616ae4
Arne Usadel <arne.usadel@googlemail.com>
2017-10-21 18:56:32 +0200

    implemented main function

--- boswatch.py
+++ boswatch.py
@@ -157,242 +157,247 @@ def get_arguments():
 #
 # Main program
 #
-try:
-    # initialization:
-    rtl_fm = None
-    multimon_ng = None
-    nmaHandler = None
-    args = get_arguments()
-
-    #
-    # Script-pathes
-    #
-    globalVars.script_path = os.path.dirname(os.path.abspath(__file__))
-
-    #
-    # Set log_path
-    #
-    if args.usevarlog:
-        globalVars.log_path = "/var/log/BOSWatch/"
-    else:
-        globalVars.log_path = os.path.join(globalVars.script_path, "log")
-
-    #
-    # If necessary create log-path
-    #
-    if not os.path.exists(globalVars.log_path):
-        os.mkdir(globalVars.log_path)
-
-    parse_config()
-    init_logging(args)
-
-    demodulation = ""
-    if "FMS" in args.demod:
-        demodulation += "-a FMSFSK "
-        logging.debug(" - Demod: FMS")
-    if "ZVEI" in args.demod:
-        demodulation += "-a ZVEI1 "
-        logging.debug(" - Demod: ZVEI")
-    if "POC512" in args.demod:
-        demodulation += "-a POCSAG512 "
-        logging.debug(" - Demod: POC512")
-    if "POC1200" in args.demod:
-        demodulation += "-a POCSAG1200 "
-        logging.debug(" - Demod: POC1200")
-    if "POC2400" in args.demod:
-        demodulation += "-a POCSAG2400 "
-        logging.debug(" - Demod: POC2400")
-
-    logging.debug(" - Use /var/log: %s", args.usevarlog)
-    logging.debug(" - Verbose Mode: %s", args.verbose)
-    logging.debug(" - Quiet Mode: %s", args.quiet)
-
-    if not args.quiet:  # only if not quiet mode
-        from includes import shellHeader
-        shellHeader.printHeader(args)
-
-    if args.test:
-        logging.warning("!!! We are in Test-Mode !!!")
-
-
-    #
-    # Add NMA logging handler
-    #
+def main():
     try:
-        if configHandler.checkConfig("NMAHandler"):
-            # is NMAHandler enabled?
-            if globalVars.config.getboolean("NMAHandler", "enableHandler") == True:
-                # we only could do something, if an APIKey is given:
-                if len(globalVars.config.get("NMAHandler","APIKey")) > 0:
-                    logging.debug("add NMA logging handler")
-                    from includes import NMAHandler
-                    if globalVars.config.get("NMAHandler", "appName") == "":
-                        nmaHandler = NMAHandler.NMAHandler(globalVars.config.get("NMAHandler","APIKey"))
-                    else:
-                        nmaHandler = NMAHandler.NMAHandler(globalVars.config.get("NMAHandler","APIKey"), globalVars.config.get("NMAHandler","appName"))
-                    nmaHandler.setLevel(globalVars.config.getint("NMAHandler","loglevel"))
-                    myLogger.addHandler(nmaHandler)
-    except:
-        # It's an error, but we could work without that stuff...
-        logging.error("cannot add NMA logging handler")
-        logging.debug("cannot add NMA logging handler", exc_info=True)
+        # initialization:
+        rtl_fm = None
+        multimon_ng = None
+        nmaHandler = None
+        args = get_arguments()
 
-    # initialization was fine, continue with main program...
+        #
+        # Script-pathes
+        #
+        globalVars.script_path = os.path.dirname(os.path.abspath(__file__))
 
-    #
-    # Load plugins
-    #
-    try:
-        from includes import pluginLoader
-        pluginLoader.loadPlugins()
-    except:
-        # we couldn't work without plugins -> exit
-        logging.critical("cannot load Plugins")
-        logging.debug("cannot load Plugins", exc_info=True)
-        exit(1)
+        #
+        # Set log_path
+        #
+        if args.usevarlog:
+            globalVars.log_path = "/var/log/BOSWatch/"
+        else:
+            globalVars.log_path = os.path.join(globalVars.script_path, "log")
 
-    #
-    # Load filters
-    #
-    try:
-        if globalVars.config.getboolean("BOSWatch", "useRegExFilter"):
-            from includes import regexFilter
-            regexFilter.loadFilters()
-    except:
-        # It's an error, but we could work without that stuff...
-        logging.error("cannot load filters")
-        logging.debug("cannot load filters", exc_info=True)
+        #
+        # If necessary create log-path
+        #
+        if not os.path.exists(globalVars.log_path):
+            os.mkdir(globalVars.log_path)
 
-    #
-    # Load description lists
-    #
-    try:
-        if globalVars.config.getboolean("FMS", "idDescribed") or globalVars.config.getboolean("ZVEI", "idDescribed") or globalVars.config.getboolean("POC", "idDescribed"):
-            from includes import descriptionList
-            descriptionList.loadDescriptionLists()
-    except:
-        # It's an error, but we could work without that stuff...
-        logging.error("cannot load description lists")
-        logging.debug("cannot load description lists", exc_info=True)
+        parse_config()
+        init_logging(args)
 
-    #
-    # Start rtl_fm
-    #
-    try:
-        if not args.test:
-            logging.debug("starting rtl_fm")
-            command = ""
-            if globalVars.config.has_option("BOSWatch", "rtl_path"):
-                command = globalVars.config.get("BOSWatch", "rtl_path")
-            command = command+"rtl_fm -d "+str(args.device)+" -f "+str(freqConverter.freqToHz(args.freq))+" -M fm -p "+str(args.error)+" -E DC -F 0 -l "+str(args.squelch)+" -g "+str(args.gain)+" -s 22050"
-            rtl_fm = subprocess.Popen(command.split(),
-                    # stdin=rtl_fm.stdout,
+        demodulation = ""
+        if "FMS" in args.demod:
+            demodulation += "-a FMSFSK "
+            logging.debug(" - Demod: FMS")
+        if "ZVEI" in args.demod:
+            demodulation += "-a ZVEI1 "
+            logging.debug(" - Demod: ZVEI")
+        if "POC512" in args.demod:
+            demodulation += "-a POCSAG512 "
+            logging.debug(" - Demod: POC512")
+        if "POC1200" in args.demod:
+            demodulation += "-a POCSAG1200 "
+            logging.debug(" - Demod: POC1200")
+        if "POC2400" in args.demod:
+            demodulation += "-a POCSAG2400 "
+            logging.debug(" - Demod: POC2400")
+
+        logging.debug(" - Use /var/log: %s", args.usevarlog)
+        logging.debug(" - Verbose Mode: %s", args.verbose)
+        logging.debug(" - Quiet Mode: %s", args.quiet)
+
+        if not args.quiet:  # only if not quiet mode
+            from includes import shellHeader
+            shellHeader.printHeader(args)
+
+        if args.test:
+            logging.warning("!!! We are in Test-Mode !!!")
+
+
+        #
+        # Add NMA logging handler
+        #
+        try:
+            if configHandler.checkConfig("NMAHandler"):
+                # is NMAHandler enabled?
+                if globalVars.config.getboolean("NMAHandler", "enableHandler") == True:
+                    # we only could do something, if an APIKey is given:
+                    if len(globalVars.config.get("NMAHandler","APIKey")) > 0:
+                        logging.debug("add NMA logging handler")
+                        from includes import NMAHandler
+                        if globalVars.config.get("NMAHandler", "appName") == "":
+                            nmaHandler = NMAHandler.NMAHandler(globalVars.config.get("NMAHandler","APIKey"))
+                        else:
+                            nmaHandler = NMAHandler.NMAHandler(globalVars.config.get("NMAHandler","APIKey"), globalVars.config.get("NMAHandler","appName"))
+                        nmaHandler.setLevel(globalVars.config.getint("NMAHandler","loglevel"))
+                        myLogger.addHandler(nmaHandler)
+        except:
+            # It's an error, but we could work without that stuff...
+            logging.error("cannot add NMA logging handler")
+            logging.debug("cannot add NMA logging handler", exc_info=True)
+
+        # initialization was fine, continue with main program...
+
+        #
+        # Load plugins
+        #
+        try:
+            from includes import pluginLoader
+            pluginLoader.loadPlugins()
+        except:
+            # we couldn't work without plugins -> exit
+            logging.critical("cannot load Plugins")
+            logging.debug("cannot load Plugins", exc_info=True)
+            exit(1)
+
+        #
+        # Load filters
+        #
+        try:
+            if globalVars.config.getboolean("BOSWatch", "useRegExFilter"):
+                from includes import regexFilter
+                regexFilter.loadFilters()
+        except:
+            # It's an error, but we could work without that stuff...
+            logging.error("cannot load filters")
+            logging.debug("cannot load filters", exc_info=True)
+
+        #
+        # Load description lists
+        #
+        try:
+            if globalVars.config.getboolean("FMS", "idDescribed") or globalVars.config.getboolean("ZVEI", "idDescribed") or globalVars.config.getboolean("POC", "idDescribed"):
+                from includes import descriptionList
+                descriptionList.loadDescriptionLists()
+        except:
+            # It's an error, but we could work without that stuff...
+            logging.error("cannot load description lists")
+            logging.debug("cannot load description lists", exc_info=True)
+
+        #
+        # Start rtl_fm
+        #
+        try:
+            if not args.test:
+                logging.debug("starting rtl_fm")
+                command = ""
+                if globalVars.config.has_option("BOSWatch", "rtl_path"):
+                    command = globalVars.config.get("BOSWatch", "rtl_path")
+                command = command+"rtl_fm -d "+str(args.device)+" -f "+str(freqConverter.freqToHz(args.freq))+" -M fm -p "+str(args.error)+" -E DC -F 0 -l "+str(args.squelch)+" -g "+str(args.gain)+" -s 22050"
+                rtl_fm = subprocess.Popen(command.split(),
+                        # stdin=rtl_fm.stdout,
+                        stdout=subprocess.PIPE,
+                        stderr=open(globalVars.log_path+"rtl_fm.log", "a"),
+                        shell=False)
+                # rtl_fm doesn't self-destruct, when an error occurs
+                # wait a moment to give the subprocess a chance to write the logfile
+                time.sleep(3)
+                checkSubprocesses.checkRTL()
+            else:
+                logging.warning("!!! Test-Mode: rtl_fm not started !!!")
+        except:
+            # we couldn't work without rtl_fm -> exit
+            logging.critical("cannot start rtl_fm")
+            logging.debug("cannot start rtl_fm", exc_info=True)
+            exit(1)
+
+        #
+        # Start multimon
+        #
+        try:
+            if not args.test:
+                logging.debug("starting multimon-ng")
+                command = ""
+                if globalVars.config.has_option("BOSWatch", "multimon_path"):
+                    command = globalVars.config.get("BOSWatch", "multimon_path")
+                command += "multimon-ng " + str(demodulation) + " -f alpha -t raw /dev/stdin - "
+                multimon_ng = subprocess.Popen(command.split(),
+                    stdin=rtl_fm.stdout,
                     stdout=subprocess.PIPE,
-                    stderr=open(globalVars.log_path+"rtl_fm.log", "a"),
+                    stderr=open(globalVars.log_path+"multimon.log","a"),
                     shell=False)
-            # rtl_fm doesn't self-destruct, when an error occurs
-            # wait a moment to give the subprocess a chance to write the logfile
-            time.sleep(3)
-            checkSubprocesses.checkRTL()
-        else:
-            logging.warning("!!! Test-Mode: rtl_fm not started !!!")
-    except:
-        # we couldn't work without rtl_fm -> exit
-        logging.critical("cannot start rtl_fm")
-        logging.debug("cannot start rtl_fm", exc_info=True)
-        exit(1)
+                # multimon-ng  doesn't self-destruct, when an error occurs
+                # wait a moment to give the subprocess a chance to write the logfile
+                time.sleep(3)
+                checkSubprocesses.checkMultimon()
+            else:
+                logging.warning("!!! Test-Mode: multimon-ng not started !!!")
+        except:
+            # we couldn't work without multimon-ng -> exit
+            logging.critical("cannot start multimon-ng")
+            logging.debug("cannot start multimon-ng", exc_info=True)
+            exit(1)
 
-    #
-    # Start multimon
-    #
-    try:
+        #
+        # Get decoded data from multimon-ng and call BOSWatch-decoder
+        #
         if not args.test:
-            logging.debug("starting multimon-ng")
-            command = ""
-            if globalVars.config.has_option("BOSWatch", "multimon_path"):
-                command = globalVars.config.get("BOSWatch", "multimon_path")
-            command += "multimon-ng " + str(demodulation) + " -f alpha -t raw /dev/stdin - "
-            multimon_ng = subprocess.Popen(command.split(),
-                stdin=rtl_fm.stdout,
-                stdout=subprocess.PIPE,
-                stderr=open(globalVars.log_path+"multimon.log","a"),
-                shell=False)
-            # multimon-ng  doesn't self-destruct, when an error occurs
-            # wait a moment to give the subprocess a chance to write the logfile
-            time.sleep(3)
-            checkSubprocesses.checkMultimon()
-        else:
-            logging.warning("!!! Test-Mode: multimon-ng not started !!!")
-    except:
-        # we couldn't work without multimon-ng -> exit
-        logging.critical("cannot start multimon-ng")
-        logging.debug("cannot start multimon-ng", exc_info=True)
-        exit(1)
-
-    #
-    # Get decoded data from multimon-ng and call BOSWatch-decoder
-    #
-    if not args.test:
-        logging.debug("start decoding")
-        while True:
-            decoded = str(multimon_ng.stdout.readline())  # Get line data from multimon stdout
-            from includes import decoder
-            decoder.decode(freqConverter.freqToHz(args.freq), decoded)
-
-            # write multimon-ng raw data
-            if globalVars.config.getboolean("BOSWatch", "writeMultimonRaw"):
-                try:
-                    rawMmOut = open(globalVars.log_path+"mm_raw.txt", "a")
-                    rawMmOut.write(decoded)
-                except:
-                    logging.warning("cannot write raw multimon data")
-                finally:
-                    rawMmOut.close()
-    else:
-        logging.debug("start testing")
-        testFile = open(globalVars.script_path+"/citest/testdata.txt", "r")
-        for testData in testFile:
-            if (len(testData.rstrip(' \t\n\r')) > 1) and ("#" not in testData[0]):
-                logging.info("Testdata: %s", testData.rstrip(' \t\n\r'))
+            logging.debug("start decoding")
+            while True:
+                decoded = str(multimon_ng.stdout.readline())  # Get line data from multimon stdout
                 from includes import decoder
-                decoder.decode(freqConverter.freqToHz(args.freq), testData)
-        logging.debug("test finished")
+                decoder.decode(freqConverter.freqToHz(args.freq), decoded)
 
-except KeyboardInterrupt:
-    logging.warning("Keyboard Interrupt")
-except SystemExit:
-    # SystemExitException is thrown if daemon was terminated
-    logging.warning("SystemExit received")
-    # only exit to call finally-block
-    exit()
-except:
-    logging.exception("unknown error")
-finally:
-    try:
-        logging.debug("BOSWatch shuting down")
-        if multimon_ng and multimon_ng.pid:
-            logging.debug("terminate multimon-ng (%s)", multimon_ng.pid)
-            multimon_ng.terminate()
-            multimon_ng.wait()
-            logging.debug("multimon-ng terminated")
-        if rtl_fm and rtl_fm.pid:
-            logging.debug("terminate rtl_fm (%s)", rtl_fm.pid)
-            rtl_fm.terminate()
-            rtl_fm.wait()
-            logging.debug("rtl_fm terminated")
-        logging.debug("exiting BOSWatch")
+                # write multimon-ng raw data
+                if globalVars.config.getboolean("BOSWatch", "writeMultimonRaw"):
+                    try:
+                        rawMmOut = open(globalVars.log_path+"mm_raw.txt", "a")
+                        rawMmOut.write(decoded)
+                    except:
+                        logging.warning("cannot write raw multimon data")
+                    finally:
+                        rawMmOut.close()
+        else:
+            logging.debug("start testing")
+            testFile = open(globalVars.script_path+"/citest/testdata.txt", "r")
+            for testData in testFile:
+                if (len(testData.rstrip(' \t\n\r')) > 1) and ("#" not in testData[0]):
+                    logging.info("Testdata: %s", testData.rstrip(' \t\n\r'))
+                    from includes import decoder
+                    decoder.decode(freqConverter.freqToHz(args.freq), testData)
+            logging.debug("test finished")
+
+    except KeyboardInterrupt:
+        logging.warning("Keyboard Interrupt")
+    except SystemExit:
+        # SystemExitException is thrown if daemon was terminated
+        logging.warning("SystemExit received")
+        # only exit to call finally-block
+        exit()
     except:
-        logging.warning("failed in clean-up routine")
-        logging.debug("failed in clean-up routine", exc_info=True)
-
+        logging.exception("unknown error")
     finally:
-        # Close Logging
-        logging.debug("close Logging")
-        # Waiting for all Threads to write there logs
-        if globalVars.config.getboolean("BOSWatch","processAlarmAsync") == True:
-            logging.debug("waiting 3s for threads...")
-            time.sleep(3)
-        logging.info("BOSWatch exit()")
-        logging.shutdown()
-        if nmaHandler:
-            nmaHandler.close()
+        try:
+            logging.debug("BOSWatch shuting down")
+            if multimon_ng and multimon_ng.pid:
+                logging.debug("terminate multimon-ng (%s)", multimon_ng.pid)
+                multimon_ng.terminate()
+                multimon_ng.wait()
+                logging.debug("multimon-ng terminated")
+            if rtl_fm and rtl_fm.pid:
+                logging.debug("terminate rtl_fm (%s)", rtl_fm.pid)
+                rtl_fm.terminate()
+                rtl_fm.wait()
+                logging.debug("rtl_fm terminated")
+            logging.debug("exiting BOSWatch")
+        except:
+            logging.warning("failed in clean-up routine")
+            logging.debug("failed in clean-up routine", exc_info=True)
+
+        finally:
+            # Close Logging
+            logging.debug("close Logging")
+            # Waiting for all Threads to write there logs
+            if globalVars.config.getboolean("BOSWatch","processAlarmAsync") == True:
+                logging.debug("waiting 3s for threads...")
+                time.sleep(3)
+            logging.info("BOSWatch exit()")
+            logging.shutdown()
+            if nmaHandler:
+                nmaHandler.close()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
